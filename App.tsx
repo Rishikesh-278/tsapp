6c88c29dc6cce290d75dc8734abaadd9eb2995ed
Rishikesh-278 <rishikeshbidkar04@gmail.com>
2023-11-09 23:05:43 +0530

    Background color now changes when we refresh the app.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  RefreshControl,
+  Text,
+  View,
+  ScrollView,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState, useCallback } from "react";
 
 export default function App() {
+  const colors = [
+    "antiquewhite",
+    "aqua",
+    "brown",
+    "cadetblue",
+    "cornflowerblue",
+    "crimson",
+    "darkorange",
+    "indigo",
+    "maroon",
+    "sienna",
+  ];
+
+  const [color, setColor] = useState<string | undefined>(undefined);
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  function getRandomColors() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    // getRandomColors(colors);
+    setColor(getRandomColors());
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 20);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SafeAreaView style={{ backgroundColor: color, height: "100%" }}>
+        <ScrollView
+          contentContainerStyle={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            backgroundColor: color,
+          }}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          <Text>Pull down from top to change the backgroundColor </Text>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +65,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
